7f25d5e9a55f02b82ca0aa684be69af8dd4ed1f5
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-10-09 09:36:41 +0700

    feat: music progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,19 @@
     const GRID_COLS = Array.apply(null, Array(SIZE)).map(() => "auto").join(" ");
     const ALBUM_PATH = "/test-album.jpg"
     
+    const musicDuration = 248;
     const musicProgressMin = 0;
     const musicProgressMax = 10;
+    const musicProgressStep = .01;
+    
+    let musicProgressInterval: ReturnType<typeof setInterval>;
     let musicProgressValue: number;
+    let musicProgressDuration = 0;
+    
     $: musicProgressPercentage = ((musicProgressValue - musicProgressMin) / (musicProgressMax - musicProgressMin)) * 100;
+    $: musicProgressDuration = (musicProgressValue / musicProgressMax) * musicDuration;
+    $: musicParsedDuration = getParsedDuration(musicProgressDuration);
+    $: musicParsedDurationNegative = getParsedDuration(musicProgressDuration, true);
     
     let position: string[][] = [];
     async function getColors(){
@@ -28,8 +37,32 @@
     function resetMusicProgress(){
         musicProgressValue = musicProgressMin;
     }
+    function startProgress() {
+        const updateInterval = (musicDuration / (musicProgressMax / musicProgressStep)) * 1000;
+    
+        musicProgressInterval = setInterval(() => {
+            musicProgressValue = Math.min(musicProgressValue + musicProgressStep, musicProgressMax);
+
+            if (musicProgressValue >= musicProgressMax) {
+                clearInterval(musicProgressInterval);
+            }
+        }, updateInterval);
+    }
+    function getParsedDuration(duration: number, negative = false): string {
+        let minutes = 0;
+        let seconds = negative ? musicDuration - duration : duration;
+        
+        while(seconds > 60){
+            minutes++;
+            seconds -= 60;
+        }
+        seconds = Math.floor(seconds);
+        return `${minutes}:${seconds < 10 ? 0 : ""}${seconds}`;
+    }
+
     resetMusicProgress()
     getColors();
+    startProgress();
 </script>
 <div class="w-screen h-screen absolute grid items-center justify-center">
     <div class="max-w-[75rem]">
@@ -38,13 +71,13 @@
                 <img src={ALBUM_PATH} class="rounded-lg" alt="Music Album">
             </div>
             <div class="w-full grid grid-cols-3">
-                <div class="text-sm flex"><span class="self-end">0:10</span></div>
+                <div class="text-sm flex"><span class="self-end">{musicParsedDuration}</span></div>
                 <div class="text-xl text-center mt-2">Dabin - Wild Youth</div>
-                <div class="text-sm flex justify-end"><span class="self-end">-0:30</span></div>
+                <div class="text-sm flex justify-end"><span class="self-end">-{musicParsedDurationNegative}</span></div>
             </div>
             <div class="w-full mt-[-4px]">
                 <input bind:value={musicProgressValue} id="music-progress-bar" style={`--progress-width: ${musicProgressPercentage}%`}
-                    class="w-full" min={musicProgressMin} max={musicProgressMax} step="0.01" type="range">
+                    class="w-full" min={musicProgressMin} max={musicProgressMax} step={musicProgressStep} type="range">
             </div>
             <div class="w-full grid grid-cols-3 mt-2">
                 <div class="flex justify-end">
