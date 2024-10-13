b54e07a6817b4b98bb1817463450e82f7d8a1b86
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-10-13 17:53:12 +0700

    refactor: seperate codes

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,21 @@
+use std::sync::Mutex;
+use tauri::State;
+
+use crate::AppState;
+
+#[tauri::command]
+pub fn music_controller(state: State<'_, Mutex<AppState>>, command: String, message: String){
+    let mut state = state.lock().unwrap();
+    state.music_player.set_path(String::from("test-music.flac")).expect("Can't change music player name");
+    if command == "play" {
+        state.music_player.play().expect("Can't play music player.");
+    } else if command == "pause" || command == "stop" {
+        state.music_player.pause().expect("Can't pause music player.");
+    }
+}
+
+#[tauri::command]
+pub fn music_set_position(state: State<'_, Mutex<AppState>>, position: u64){
+    let mut state = state.lock().unwrap();
+    state.music_player.set_pos(position).expect("Can't set music player position");
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,9 @@
 use std::sync::Mutex;
-use tauri::{Builder, Manager, State};
+use tauri::{Builder, Manager};
 use music::MusicPlayer;
 
 mod music;
-
-#[tauri::command]
-fn music_controller(state: State<'_, Mutex<AppState>>, command: String){
-    let mut state = state.lock().unwrap();
-    state.music_player.set_path(String::from("test-music.flac")).expect("Can't change music player name");
-    if command == "play" {
-        state.music_player.play().expect("Can't play music player.");
-    } else if command == "pause" || command == "stop" {
-        state.music_player.pause().expect("Can't pause music player.");
-    }
-}
-
-#[tauri::command]
-fn music_set_position(state: State<'_, Mutex<AppState>>, position: u64){
-    let mut state = state.lock().unwrap();
-    state.music_player.set_pos(position).expect("Can't set music player position");
-}
+mod commands;
 
 struct AppState {
     music_player: MusicPlayer
@@ -39,7 +23,8 @@ pub fn run() {
         })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            music_controller, music_set_position
+            commands::music_controller,
+            commands::music_set_position
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
