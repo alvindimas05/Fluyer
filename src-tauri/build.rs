028a79a593dabeba35960f6d4b88c3e199d937f5
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-12-17 18:31:00 +0700

    chore: add note

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,7 @@
 fn main() {
-    // https://kazlauskas.me/entries/writing-proper-buildrs-scripts
-    println!("cargo:rustc-link-lib=dylib=stdc++");
-    println!("cargo:rustc-link-lib=c++_shared");
+    // Note: This is for Android build only. Uncomment when required.
+    // Source: https://kazlauskas.me/entries/writing-proper-buildrs-scripts
+    // println!("cargo:rustc-link-lib=dylib=stdc++");
+    // println!("cargo:rustc-link-lib=c++_shared");
     tauri_build::build()
 }
