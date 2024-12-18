71e69d8a5969fb588c62d577beb78e6bdff5559d
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-12-18 11:19:50 +0700

    feat: android transparent status bar

--- src-tauri/gen/android/app/src/main/java/org/alvindimas05/fluyer/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/org/alvindimas05/fluyer/MainActivity.kt
@@ -1,3 +1,13 @@
 package org.alvindimas05.fluyer
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
 
-class MainActivity : TauriActivity()
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
