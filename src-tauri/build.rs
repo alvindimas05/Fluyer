150c9a30440440c17a39f9c7d2594b5adcedc3a5
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-12-21 19:30:25 +0700

    chore: dead code

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,16 +1,4 @@
-// use tauri_build::WindowsAttributes;
-
 fn main() {
-    // tauri_build::try_build(
-    //     tauri_build::Attributes::new()
-    //         .windows_attributes(WindowsAttributes::new_without_app_manifest())
-    //         .plugin(
-    //             "tauri-plugin-fluyer",
-    //             tauri_build::InlinedPlugin::new().commands(&["toast"]),
-    //         ),
-    // )
-    // .expect("Failed to build Fluyer plugin.");
-
     // Note: Used for Rodio because it requires C++ libraries.
     // Source: https://kazlauskas.me/entries/writing-proper-buildrs-scripts
     let target_os = std::env::var("CARGO_CFG_TARGET_OS");
