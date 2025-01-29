11455671d0f56629fddb131e6b733612987b33d9
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-01-29 14:54:36 +0700

    fix: disable auto restart to prevent false alarm

--- src/lib/mobile/HeadsetChange.svelte
+++ src/lib/mobile/HeadsetChange.svelte
@@ -5,22 +5,22 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 
 let show = $state(false);
-let seconds = $state(10);
-let interval: ReturnType<typeof setInterval> | null = null;
+// let seconds = $state(10);
+// let interval: ReturnType<typeof setInterval> | null = null;
 
-function restartApp() {
-	invoke("plugin:fluyer|restart_app");
-}
+// function restartA/pp() {
+	// invoke("plugin:fluyer|restart_app");
+// }
 
 listen(CommandsRoute.MUSIC_HEADSET_CHANGE, () => {
 	show = true;
-	interval = setInterval(() => {
-		if (seconds > 0) seconds--;
-		else {
-			clearInterval(interval!);
-			restartApp();
-		}
-	}, 1000);
+	// interval = setInterval(() => {
+	// 	if (seconds > 0) seconds--;
+	// 	else {
+	// 		clearInterval(interval!);
+	// 		restartApp();
+	// 	}
+	// }, 1000);
 });
 
 MusicController.pause();
@@ -39,7 +39,7 @@ MusicController.pause();
             <p class="mt-2">
                 We apologize for the inconvenience. Due to current package
                 limitations, a restart is required to restore sound
-                functionality. The app will restart automatically in {seconds} seconds.
+                functionality.
             </p>
             <button
                 class="border border-gray-300 bg-gray-500 bg-opacity-10 hover:bg-opacity-20 rounded w-fit mt-5 p-2"
