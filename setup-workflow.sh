d83fafe336b46ced866e46bd3371e1103a2c659a
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-02-01 17:57:43 +0700

    chore: oops some typos

--- setup-workflow.sh
+++ setup-workflow.sh
@@ -2,18 +2,21 @@
 
 if [[ "$os" == "windows" || "$os" == "macos" ]]; then
     bun i
-else if [[ "$os" == "linux" ]; then
+elif [[ "$os" == "linux" ]]; then
     apt-get update
     apt-get install -y libwebkit2gtk-4.1-dev libappindicator3-dev librsvg2-dev patchelf librust-alsa-sys-dev
     bun i
-else if [[ "$os" == "android" ]]; then
+elif [[ "$os" == "android" ]]; then
     bun i
-    cd src-tauri/gen/android
-    echo "keyAlias=$ANDROID_KEY_ALIAS" > keystore.properties
-    echo "password=$ANDROID_KEY_PASSWORD" >> keystore.properties
-    base64 -d <<< "$ANDROID_KEY_BASE64" > $RUNNER_TEMP/keystore.jks
-    echo "storeFile=$RUNNER_TEMP/keystore.jks" >> keystore.properties
+    cd src-tauri/gen/android || exit
+    {
+        echo "keyAlias=$ANDROID_KEY_ALIAS"
+        echo "password=$ANDROID_KEY_PASSWORD"
+        base64 -d <<< "$ANDROID_KEY_BASE64" > "$RUNNER_TEMP/keystore.jks"
+        echo "storeFile=$RUNNER_TEMP/keystore.jks"
+    } > keystore.properties
     cd ../../../
-    bun tauri android build -v --target $ANDROID_ARCH
-    mkdir apks
-    mv ./src-tauri/gen/android/app/build/outputs/apk/universal/release/app-universal-release.apk ./apks/Fluyer_$APP_VERSION_$ANDROID_ARCH.apk
+    bun tauri android build -v --target "$ANDROID_ARCH"
+    mkdir -p apks
+    mv "./src-tauri/gen/android/app/build/outputs/apk/universal/release/app-universal-release.apk" "./apks/Fluyer_${APP_VERSION}_${ANDROID_ARCH}.apk"
+fi
