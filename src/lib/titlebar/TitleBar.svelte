e7a13e87fdacaea59cbe6b472e7b107c4d10b7ce
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-06-15 23:11:28 +0700

    feat: linux titlebar icons

--- src/lib/titlebar/TitleBar.svelte
+++ src/lib/titlebar/TitleBar.svelte
@@ -1,69 +1,105 @@
 <script lang="ts">
-import { isWindows } from "$lib/platform";
-import { getCurrentWindow } from "@tauri-apps/api/window";
-import { invoke } from "@tauri-apps/api/core";
-import { PageRoutes } from "$lib/pages";
-import { afterNavigate } from "$app/navigation";
+    import { isDesktop, isLinux, isWindows } from "$lib/platform";
+    import { getCurrentWindow } from "@tauri-apps/api/window";
+    import { invoke } from "@tauri-apps/api/core";
+    import { PageRoutes } from "$lib/pages";
+    import { afterNavigate } from "$app/navigation";
 
-let isMaximized = $state(true);
-let isPlayPage = $state(false);
-const window = getCurrentWindow();
-function onMouseDown(
-	e: MouseEvent & {
-		currentTarget: EventTarget & HTMLDivElement;
-	},
-) {
-	if (e.buttons === 1) {
-		e.detail === 2 ? window.toggleMaximize() : window.startDragging();
-	}
-}
+    const LINUX_ICONS = {
+        close: "/icons/linux/window-close-symbolic.svg",
+        maximize: "/icons/linux/window-maximize-symbolic.svg",
+        minimize: "/icons/linux/window-minimize-symbolic.svg",
+        restore: "/icons/linux/window-restore-symbolic.svg",
+    };
 
-let snapOverlayTimer: ReturnType<typeof setTimeout> | null = null;
-function showSnapOverlay() {
-	window.setFocus().then(() => invoke("decorum_show_snap_overlay"));
-}
+    let isMaximized = $state(true);
+    let isPlayPage = $state(false);
+    const window = getCurrentWindow();
+    function onMouseDown(
+        e: MouseEvent & {
+            currentTarget: EventTarget & HTMLDivElement;
+        },
+    ) {
+        if (e.buttons === 1) {
+            e.detail === 2 ? window.toggleMaximize() : window.startDragging();
+        }
+    }
 
-function handleMaximizeMouseEnter() {
-	if (!isWindows()) return;
-	snapOverlayTimer = setTimeout(showSnapOverlay, 620);
-}
+    let snapOverlayTimer: ReturnType<typeof setTimeout> | null = null;
+    function showSnapOverlay() {
+        window.setFocus().then(() => invoke("decorum_show_snap_overlay"));
+    }
 
-function handleMaximizeMouseLeave() {
-	if (!isWindows()) return;
-	if (snapOverlayTimer != null) clearTimeout(snapOverlayTimer);
-}
+    function handleMaximizeMouseEnter() {
+        if (!isWindows()) return;
+        snapOverlayTimer = setTimeout(showSnapOverlay, 620);
+    }
 
-window.onResized(async (_) => {
-	isMaximized = await window.isMaximized();
-});
+    function handleMaximizeMouseLeave() {
+        if (!isWindows()) return;
+        if (snapOverlayTimer != null) clearTimeout(snapOverlayTimer);
+    }
 
-afterNavigate((navigation) => {
-	isPlayPage = navigation.to?.route.id === PageRoutes.PLAY;
-});
+    window.onResized(async (_) => {
+        isMaximized = await window.isMaximized();
+    });
+
+    afterNavigate((navigation) => {
+        isPlayPage = navigation.to?.route.id === PageRoutes.PLAY;
+    });
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class="fixed top-0 left-0 w-full h-8 z-[99999] grid grid-cols-[1fr_auto]">
     <div class="h-full w-full" onmousedown={onMouseDown}></div>
-    {#if isWindows() && !isPlayPage}
+    {#if (isWindows() || isLinux()) && !isPlayPage}
         <div class="pe-3">
-            <button class="tb-button" 
-                onclick={() => window.minimize()}>
-                &#59681;
-            </button>
-            <button class="tb-button" 
-                onmouseenter={handleMaximizeMouseEnter}
-                onmouseleave={handleMaximizeMouseLeave}
-                onclick={() => window.toggleMaximize()}>
-                {#if isMaximized}
-                    &#59683;
-                {:else}
-                    &#59682;
+            <button
+                class={`tb-button ${isWindows() && "win-button"} ${isLinux() && "linux-button"}`}
+                onclick={() => window.minimize()}
+            >
+                {#if isWindows()}
+                    &#59681;
+                {/if}
+                {#if isLinux()}
+                    <!-- svelte-ignore a11y_missing_attribute -->
+                    <img src={LINUX_ICONS.minimize} />
                 {/if}
             </button>
-            <button class="tb-button" 
-                onclick={() => window.close()}>
-                &#59579;
+            <button
+                class={`tb-button ${isWindows() && "win-button"} ${isLinux() && "linux-button"}`}
+                onmouseenter={handleMaximizeMouseEnter}
+                onmouseleave={handleMaximizeMouseLeave}
+                onclick={() => window.toggleMaximize()}
+            >
+                {#if isWindows()}
+                    {#if isMaximized}
+                        &#59683;
+                    {:else}
+                        &#59682;
+                    {/if}
+                {/if}
+                {#if isLinux()}
+                    {#if isMaximized}
+                        <!-- svelte-ignore a11y_missing_attribute -->
+                        <img src={LINUX_ICONS.restore} />
+                    {:else}
+                        <!-- svelte-ignore a11y_missing_attribute -->
+                        <img src={LINUX_ICONS.maximize} />
+                    {/if}
+                {/if}
+            </button>
+            <button
+                class={`tb-button ${isWindows() && "win-button"} ${isLinux() && "linux-button"}`}
+                onclick={() => window.close()}
+            >
+                {#if isWindows()}
+                    &#59579;
+                {/if}
+                {#if isLinux()}
+                    <!-- svelte-ignore a11y_missing_attribute -->
+                    <img src={LINUX_ICONS.close} />
+                {/if}
             </button>
         </div>
     {/if}
@@ -71,10 +107,19 @@ afterNavigate((navigation) => {
 
 <style lang="scss">
     .tb-button {
-        @apply hover:bg-gray-300/25 py-2 px-3 rounded text-[10px] font-[300];
-		transition: background 0.1s;
-		text-rendering: optimizeLegibility;
-		-webkit-font-smoothing: antialiased;
-		font-family: 'Segoe Fluent Icons', 'Segoe MDL2 Assets';
+        @apply hover:bg-gray-300/25 rounded text-[10px] font-[300];
+    }
+    .win-button {
+        @apply py-2 px-3;
+        transition: background 0.1s;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        font-family: "Segoe Fluent Icons", "Segoe MDL2 Assets";
+    }
+    .linux-button {
+        @apply p-1;
+        img {
+            width: 1.25rem;
+        }
     }
 </style>
