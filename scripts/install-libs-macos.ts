93bec7f0309368522d1d4e08a1d6ce5af84ff69f
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-06-15 14:44:22 +0700

    fix: macos arm64 libs

--- scripts/install-libs-macos.ts
+++ scripts/install-libs-macos.ts
@@ -1,16 +1,34 @@
 import path from "path";
 import { downloadFile, extractZip } from "./install-helpers";
 import fs from "fs/promises";
+import axios from "axios";
 
+const confPath = path.resolve("src-tauri", "tauri.macos.conf.json");
 const destPath = path.resolve("src-tauri", "libs");
-const zipPath = path.resolve(destPath, "macos-libs.zip");
+const arch = process.arch === 'arm64' ? 'arm64' : 'x86_64';
+const zipName = `macos-libs-${arch}.zip`;
+const zipPath = path.resolve(destPath, zipName);
+
+const filelistURL = `https://raw.githubusercontent.com/alvindimas05/Fluyer-API/refs/heads/main/filelist-${arch}.txt`;
+
+async function updateConfWithFileList() {
+	const res = await axios.get(filelistURL);
+	const lines = res.data.split("\n").filter((line: string) => line.trim() !== "");
+	const libPaths = lines.map((file: string) => `./libs/${file}`);
+
+	const json = { bundle: { macOS: { frameworks: libPaths } } };
+
+	await fs.writeFile(confPath, JSON.stringify(json, null, 4));
+	console.log(`Updated conf with dylib paths at ${confPath}.`);
+}
+
 async function main() {
 	// Assume it only contains .gitignore
 	if ((await fs.readdir(destPath)).length > 1) return;
 
 	console.log("Installing MacOS libs...");
 	await downloadFile(
-		"https://github.com/alvindimas05/Fluyer-API/releases/latest/download/macos-libs.zip",
+		`https://github.com/alvindimas05/Fluyer-API/releases/latest/download/${zipName}`,
 		zipPath,
 	);
 	await extractZip(zipPath, destPath);
@@ -18,6 +36,8 @@ async function main() {
 		path.resolve(destPath, "libmpv.2.dylib"),
 		path.resolve(destPath, "libmpv.dylib"),
 	);
+
+	await updateConfWithFileList();
 }
 
 main();
