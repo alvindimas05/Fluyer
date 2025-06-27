92cc19390b530f1f9e674770cb544962f187266d
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-06-28 00:37:49 +0700

    fix: optimize playlist drag

--- src/lib/home/playlist/Playlist.svelte
+++ src/lib/home/playlist/Playlist.svelte
@@ -1,61 +1,98 @@
 <script lang="ts">
-    import Sidebar from "$lib/home/sidebar/Sidebar.svelte";
-    import { musicCurrentIndex, musicPlaylist } from "$lib/stores/music";
-    import PlaylistItem from "./PlaylistItem.svelte";
-    import { SidebarType } from "$lib/home/sidebar/types";
-    import Icon from "$lib/icon/Icon.svelte";
-    import { IconType } from "$lib/icon/types";
-    import MusicController from "$lib/controllers/MusicController";
-    import Muuri from "muuri";
-    import { onMount } from "svelte";
-    import {isMobile} from "$lib/platform";
+import Sidebar from "$lib/home/sidebar/Sidebar.svelte";
+import { musicPlaylist } from "$lib/stores/music";
+import PlaylistItem from "./PlaylistItem.svelte";
+import { SidebarType } from "$lib/home/sidebar/types";
+import Icon from "$lib/icon/Icon.svelte";
+import { IconType } from "$lib/icon/types";
+import MusicController from "$lib/controllers/MusicController";
+import Muuri from "muuri";
+import {mount, onMount, unmount} from "svelte";
+import {isMobile} from "$lib/platform";
+import type {MusicData} from "$lib/home/music/types";
 
-    let gridElement: HTMLDivElement;
-    let muuri: Muuri;
-    let dragging = $state(!isMobile());
+let gridElement: HTMLDivElement;
+let muuri: Muuri;
+let dragging = $state(!isMobile());
+let oldPlaylist: MusicData[] = [];
+let fromIndex = $state(-1);
 
-    function cleanPlaylist() {
-        MusicController.reset();
-    }
+function cleanPlaylist() {
+    MusicController.reset();
+}
 
-    function dragToggle() {
-        dragging = !dragging;
-    }
+function dragToggle() {
+    dragging = !dragging;
+}
 
-    function initMuuri() {
-        if (muuri) muuri.destroy();
-        muuri = new Muuri(gridElement, {
-            dragEnabled: dragging,
-            dragSortPredicate: {
-                action: 'move'
-            },
-        });
+function initMuuri() {
+    muuri = new Muuri(gridElement, {
+        dragEnabled: true,
+        dragSortPredicate: {
+            action: 'move'
+        },
+        dragStartPredicate: () => dragging,
+    });
 
-        muuri.on('dragEnd', (item, _) => {
-            if (!muuri || !$musicPlaylist) return;
+    muuri.on('dragStart', (item, _) => {
+        if (!$musicPlaylist) return;
+        fromIndex = muuri.getItems().indexOf(item);
+    });
+
+    muuri.on('dragEnd', (item, _) => {
+        if (!muuri || !$musicPlaylist) return;
+        const toIndex = muuri.getItems().indexOf(item);
+
+        if(fromIndex === toIndex) return;
+
+        MusicController.playlistMoveto(fromIndex, toIndex);
+    });
+
+    return muuri;
+}
+
+function createPlaylistItem(music: MusicData) {
+    const element = document.createElement('div');
+    element.className = 'absolute w-full h-fit';
+    mount(PlaylistItem, { target: element, props: { music }});
+    return element;
+}
+
+onMount(() => {
+    initMuuri();
+
+    musicPlaylist.subscribe((playlist) => {
+        if (playlist.length < 1) return;
+
+        const removedIndices = oldPlaylist
+            .map((music, index) => !playlist.includes(music) ? index : -1)
+            .filter(index => index !== -1);
+
+        if (removedIndices.length > 0) {
             const items = muuri.getItems();
-            const element = item.getElement()!!;
-            const fromIndex = parseInt(element.getAttribute('data-index') || '0');
-            const toIndex = items.indexOf(item);
+            const removedItems = removedIndices.map(i => items[i]);
+            for (const item of removedItems) {
+                unmount(item.getElement()!!);
+            }
+            muuri.remove(removedItems, {removeElements: true});
+        }
 
-            if(fromIndex === toIndex) return;
+        const newItems = playlist
+            .map((music, index) => !oldPlaylist.includes(music) ? {music, index} : null)
+            .filter((item): item is {music: MusicData, index: number} => item !== null);
 
-            MusicController.playlistMoveto(fromIndex, toIndex);
-        });
+        if (newItems.length > 0) {
+            muuri.add(newItems.map(({music}) => createPlaylistItem(music)));
+        }
 
-        return muuri;
-    }
-
-    onMount(() => {
-        initMuuri();
-
-        // FIXME: Dirty method, might fix later
-        musicPlaylist.subscribe(() => setTimeout(initMuuri));
+        oldPlaylist = [...playlist];
     });
-    $effect(() => {
-        dragging;
-        initMuuri();
-    });
+});
+
+$effect(() => {
+    dragging;
+    initMuuri();
+});
 </script>
 
 <Sidebar type={SidebarType.Right}>
@@ -74,16 +111,7 @@
             </button>
         {/if}
     </div>
-    <div class="flex-1 w-full overflow-auto">
-        <div class="w-full relative" bind:this={gridElement}>
-            {#each $musicPlaylist as music, index}
-                <PlaylistItem
-                    {music}
-                    {index}
-                    isPlaying={$musicCurrentIndex === index}
-                    isPrevious={index < $musicCurrentIndex}
-                />
-            {/each}
-        </div>
+    <div class="flex-1 w-full overflow-auto scrollbar-hidden">
+        <div class="w-full relative" bind:this={gridElement}></div>
     </div>
 </Sidebar>
--- src/lib/home/playlist/PlaylistItem.svelte
+++ src/lib/home/playlist/PlaylistItem.svelte
@@ -3,20 +3,17 @@ import type { MusicData } from "../music/types";
 
 interface Props {
 	music: MusicData;
-	index: number;
-	isPlaying?: boolean;
-	isPrevious?: boolean;
 }
 import MusicController from "$lib/controllers/MusicController";
 import Icon from "$lib/icon/Icon.svelte";
 import { IconType } from "$lib/icon/types";
+import {musicCurrentIndex, musicPlaylist} from "$lib/stores/music";
 
-let {
-	music,
-	index = -1,
-	isPlaying = false,
-	isPrevious = false,
-}: Props = $props();
+let { music }: Props = $props();
+
+let index = $derived($musicPlaylist.findIndex((m) => m.path === music.path));
+let isPlaying = $derived($musicCurrentIndex === index);
+let isPrevious = $derived(index < $musicCurrentIndex);
 
 function removePlaylist() {
 	MusicController.removeMusic(index);
@@ -27,63 +24,61 @@ function gotoPlaylist() {
 }
 </script>
 
-<div class="absolute w-full h-fit" data-index={index}>
-    <div
-        class={`relative grid grid-cols-[max-content_auto_max-content] py-2 px-3 animate__animated animate__fadeIn
-            ${isPlaying && "bg-gray-700 bg-opacity-40"}`}
-    >
-        <div class="w-11 md:w-12 lg:w-14">
-            <img
-                class="w-full rounded"
-                src={MusicController.getAlbumImageFromMusic(music)}
-                alt="Album"
-            />
-        </div>
-        <div class="ms-3 text-sm md:text-base">
-            <p class="font-medium">{music.title}</p>
-            <p class="text-opacity-background-70">
-                {MusicController.getFullArtistFromMusic(music)}
-            </p>
-        </div>
-        <div class="w-11 md:w-12 lg:w-14"></div>
+<div
+    class={`relative grid grid-cols-[max-content_auto_max-content] py-2 px-3 animate__animated animate__fadeIn
+        ${isPlaying && "bg-gray-700 bg-opacity-40"}`}
+>
+    <div class="w-11 md:w-12 lg:w-14">
+        <img
+            class="w-full rounded"
+            src={MusicController.getAlbumImageFromMusic(music)}
+            alt="Album"
+        />
     </div>
-    {#if isPlaying}
-        <div
-            class="absolute top-0 left-0 w-full grid grid-cols-[max-content_auto_max-content] py-2 px-3 z-10"
-        >
-            <div class="w-11 md:w-12 lg:w-14 aspect-square"></div>
-            <div></div>
-            <div class="w-11 md:w-12 lg:w-14 aspect-square p-1 lg:p-3">
-                <div class="w-full animate__animated animate__infinite animate__pulse">
-                    <Icon type={IconType.Playing} />
-                </div>
+    <div class="ms-3 text-sm md:text-base">
+        <p class="font-medium">{music.title}</p>
+        <p class="text-opacity-background-70">
+            {MusicController.getFullArtistFromMusic(music)}
+        </p>
+    </div>
+    <div class="w-11 md:w-12 lg:w-14"></div>
+</div>
+{#if isPlaying}
+    <div
+        class="absolute top-0 left-0 w-full grid grid-cols-[max-content_auto_max-content] py-2 px-3 z-10"
+    >
+        <div class="w-11 md:w-12 lg:w-14 aspect-square"></div>
+        <div></div>
+        <div class="w-11 md:w-12 lg:w-14 aspect-square p-1 lg:p-3">
+            <div class="w-full animate__animated animate__infinite animate__pulse">
+                <Icon type={IconType.Playing} />
             </div>
         </div>
-    {:else}
-        <div
-            class="absolute top-0 left-0 w-full grid grid-cols-[max-content_auto_max-content] py-2 px-3 z-10
-            animate__animated animate__faster animate__fadeOut playlist-item-controls"
+    </div>
+{:else}
+    <div
+        class="absolute top-0 left-0 w-full grid grid-cols-[max-content_auto_max-content] py-2 px-3 z-10
+        animate__animated animate__faster animate__fadeOut playlist-item-controls"
+    >
+        <button
+            class="w-11 md:w-12 lg:w-14 aspect-square"
+            onclick={gotoPlaylist}
         >
-            <button
-                class="w-11 md:w-12 lg:w-14 aspect-square"
-                onclick={gotoPlaylist}
-            >
-                {#if !isPlaying}
-                    <div
-                        class="w-full bg-black bg-opacity-40 rounded lg:p-1"
-                    >
-                        <Icon type={isPrevious ? IconType.Previous : IconType.Next} />
-                    </div>
-                {/if}
-            </button>
-            <div></div>
-            <button class="w-11 md:w-12 lg:w-14 aspect-square lg:p-1"
-                onclick={removePlaylist}>
-                <Icon type={IconType.Remove}/>
-            </button>
-        </div>
-    {/if}
-</div>
+            {#if !isPlaying}
+                <div
+                    class="w-full bg-black bg-opacity-40 rounded lg:p-1"
+                >
+                    <Icon type={isPrevious ? IconType.Previous : IconType.Next} />
+                </div>
+            {/if}
+        </button>
+        <div></div>
+        <button class="w-11 md:w-12 lg:w-14 aspect-square lg:p-1"
+            onclick={removePlaylist}>
+            <Icon type={IconType.Remove}/>
+        </button>
+    </div>
+{/if}
 
 <style lang="scss">
     .playlist-item-controls:hover {
