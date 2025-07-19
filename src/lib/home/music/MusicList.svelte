8e1fe3e93814227834c6b8290937f4d988c13f1f
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-07-20 02:43:01 +0700

    fix: xhdpi size

--- src/lib/home/music/MusicList.svelte
+++ src/lib/home/music/MusicList.svelte
@@ -7,7 +7,8 @@ import MusicItem from "./MusicItem.svelte";
     <div
     	class="text-white grid gap-x-3
     	md-mdpi:grid-cols-2 lg-mdpi:grid-cols-3 xl-mdpi:grid-cols-4
-		md-hdpi:grid-cols-2 xl-hdpi:grid-cols-3"
+		md-hdpi:grid-cols-2 xl-hdpi:grid-cols-3
+		md-xhdpi:grid-cols-2 lg-xhdpi:grid-cols-3"
         >
     	{#if Array.isArray($musicList)}
     		{#each $musicList as music}
