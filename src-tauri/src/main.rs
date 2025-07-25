657ebaf7f566a25f1e731298a73e02200b7d59d9
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-07-25 17:44:54 +0700

    fix: log permission condition

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod logger;
+mod platform;
 
 use simplelog::{
     ColorChoice, CombinedLogger, LevelFilter, TermLogger, TerminalMode, WriteLogger,
@@ -17,29 +18,37 @@ fn main() {
 
 #[cfg(desktop)]
 fn init_logging() {
-    use simplelog::ConfigBuilder;
+    use simplelog::{ConfigBuilder, SharedLogger};
     let log_path = logger::get_log_path();
 
     let mut config = ConfigBuilder::new();
     config.add_filter_ignore_str("symphonia_core");
     let config = config.build();
 
-    CombinedLogger::init(vec![
-        WriteLogger::new(
-            LevelFilter::Warn,
-            config.clone(),
-            File::create(log_path.clone()).unwrap(),
-        ),
+    let mut logs: Vec<Box<dyn SharedLogger>> = vec![
         TermLogger::new(
             if cfg!(debug_assertions) {
                 LevelFilter::Debug
             } else {
                 LevelFilter::Warn
             },
-            config,
+            config.clone(),
             TerminalMode::Mixed,
             ColorChoice::Auto,
         )
-    ]).unwrap();
-    println!("The log file is saved at {}", log_path);
+    ];
+
+    let log_file = File::create(log_path.clone());
+    if log_file.is_ok() {
+        logs.push(
+            WriteLogger::new(
+                LevelFilter::Warn,
+                config,
+                log_file.unwrap(),
+            )
+        );
+    }
+
+    CombinedLogger::init(logs).unwrap();
+    logger::debug!("The log file is saved at {}", log_path);
 }
