c5e0426b1871ca65263cc45cd3ff3100845e0d17
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-08-03 14:25:35 +0700

    fix: android build

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,7 @@
 use std::env;
-
 fn main() {
-    #[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
-    println!("cargo:rustc-link-search=native={}", std::env::var("FLUYER_MPV_SOURCE").unwrap());
+    if !std::env::var("TARGET").unwrap().contains("android"){
+        println!("cargo:rustc-link-search=native={}", std::env::var("FLUYER_MPV_SOURCE").unwrap());
+    }
     tauri_build::build()
 }
