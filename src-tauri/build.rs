7f4b0f68bfcba687e8073a492682e0e54f2cdafa
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-08-03 14:12:06 +0700

    fix: build condition

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    #[cfg(any(desktop, target_os = "windows"))]
+    #[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
     println!("cargo:rustc-link-search=native={}", std::env::var("FLUYER_MPV_SOURCE").unwrap());
     tauri_build::build()
 }
