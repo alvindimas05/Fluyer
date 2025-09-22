3a52e320f02090b850a610fd2fb5d58fce4f537c
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-09-22 15:05:57 +0700

    chore: tabs

--- src/lib/settings/SettingInput.svelte
+++ src/lib/settings/SettingInput.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    interface Props {
-        children?: import("svelte").Snippet;
-    }
+interface Props {
+    children?: import("svelte").Snippet;
+}
 
-    let { children }: Props = $props();
+let { children }: Props = $props();
 </script>
 <div class="bg-gradient-to-r from-white/15 to-white/10 rounded-lg shadow-md my-3 mx-5">
     {@render children?.()}
