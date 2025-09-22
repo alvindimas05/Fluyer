cd9fc22cabe92f88763df4fd73ba7161b92ab197
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2025-09-22 15:14:49 +0700

    fix: settings input styling

--- src/lib/settings/SettingInput.svelte
+++ src/lib/settings/SettingInput.svelte
@@ -5,6 +5,6 @@ interface Props {
 
 let { children }: Props = $props();
 </script>
-<div class="bg-gradient-to-r from-white/15 to-white/10 rounded-lg shadow-md my-3 mx-5">
+<div class="bg-white/5 border border-white/30 rounded-lg shadow-md my-3 mx-5">
     {@render children?.()}
 </div>
