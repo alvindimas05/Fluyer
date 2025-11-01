b9163ed8fb6885dbb6debbbcc5867451534c0cdb
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-11-02 00:30:11 +0700

    fix: satisfy build.rs for rust analyzer and cargo

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,19 @@
 fn main() {
     if !std::env::var("TARGET").unwrap().contains("android") {
-        // if let Ok(source) = std::env::var("FLUYER_MPV_SOURCE") {
-        //     println!("cargo:rustc-link-search=native={}", source);
-        // }
-        println!(
-            "cargo:rustc-link-search=native={}",
-            std::env::var("FLUYER_MPV_SOURCE").unwrap()
-        );
+        // Note: Required to satisfy both rust-analyzer and cargo run or build
+        // For some reason, cargo can't detect the Ok() condition.
+        // So we need to condition it if the condition works or not
+        let mut is_linked = false;
+        if let Ok(source) = std::env::var("FLUYER_MPV_SOURCE") {
+            is_linked = true;
+            println!("cargo:rustc-link-search=native={}", source);
+        }
+        if !is_linked {
+            println!(
+                "cargo:rustc-link-search=native={}",
+                std::env::var("FLUYER_MPV_SOURCE").unwrap()
+            );
+        }
     }
     tauri_build::build()
 }
