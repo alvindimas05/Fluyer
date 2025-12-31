86c1726a818aecb626c64011335d4a0dcc96f972
alvindimas05 <alvin.dimas.praditya@gmail.com>
2025-12-31 10:52:05 +0700

    fix: forgot to remove blur code

--- src/lib/ui/glass/Glass.svelte
+++ src/lib/ui/glass/Glass.svelte
@@ -1,53 +1,54 @@
 <script lang="ts">
-import { isAndroid } from "$lib/platform";
-import type {GlassShineSize} from "$lib/ui/glass/types";
+	import { isAndroid } from "$lib/platform";
+	import type { GlassShineSize } from "$lib/ui/glass/types";
 
-interface Props {
-	children?: import("svelte").Snippet;
-	class?: string;
-	style?: string;
-	showShine?: boolean;
-	shineColor?: string;
-	showShadow?: boolean;
-	enableBlur?: boolean;
-	thisElement?: HTMLDivElement;
-    shineSize?: GlassShineSize;
-	events?: any;
-}
+	interface Props {
+		children?: import("svelte").Snippet;
+		class?: string;
+		style?: string;
+		showShine?: boolean;
+		shineColor?: string;
+		showShadow?: boolean;
+		enableBlur?: boolean;
+		thisElement?: HTMLDivElement;
+		shineSize?: GlassShineSize;
+		events?: any;
+	}
 
-let {
-	children,
-	shineColor = "rgba(255, 255, 255, 0.5)",
-	enableBlur = false,
-    shineSize = "md",
-	thisElement = $bindable<HTMLDivElement>(),
-	...props
-}: Props = $props();
+	let {
+		children,
+		shineColor = "rgba(255, 255, 255, 0.5)",
+		enableBlur = false,
+		shineSize = "md",
+		thisElement = $bindable<HTMLDivElement>(),
+		...props
+	}: Props = $props();
 
-const getBlurClass = () => {
-	// if (!enableBlur) return "";
-	// return isAndroid() ? "backdrop-blur-xs" : "backdrop-blur-md";
-	return "";
-};
+	const getBlurClass = () => {
+		if (!enableBlur) return "";
+		return isAndroid() ? "backdrop-blur-xs" : "backdrop-blur-md";
+	};
 
-const getHoverClasses = () => {
-	return "transition-all duration-[400ms] ease-[cubic-bezier(0.175,0.885,0.32,2.2)]";
-};
+	const getHoverClasses = () => {
+		return "transition-all duration-[400ms] ease-[cubic-bezier(0.175,0.885,0.32,2.2)]";
+	};
 
-const getShineSize = () => {
-    if (shineSize === "sm")
-        return "shadow-[inset_1px_1px_0.5px_0_var(--shine-color),inset_-1px_-1px_0.5px_0_var(--shine-color),0_6px_8px_-3px_rgb(0_0_0_/_0.1),0_3px_4px_-3px_rgb(0_0_0_/_0.1)]";
-    return "shadow-[inset_2px_2px_1px_0_var(--shine-color),inset_-1px_-1px_1px_1px_var(--shine-color),0_10px_15px_-3px_rgb(0_0_0_/_0.1),0_4px_6px_-4px_rgb(0_0_0_/_0.1)]";
-};
+	const getShineSize = () => {
+		if (shineSize === "sm")
+			return "shadow-[inset_1px_1px_0.5px_0_var(--shine-color),inset_-1px_-1px_0.5px_0_var(--shine-color),0_6px_8px_-3px_rgb(0_0_0_/_0.1),0_3px_4px_-3px_rgb(0_0_0_/_0.1)]";
+		return "shadow-[inset_2px_2px_1px_0_var(--shine-color),inset_-1px_-1px_1px_1px_var(--shine-color),0_10px_15px_-3px_rgb(0_0_0_/_0.1),0_4px_6px_-4px_rgb(0_0_0_/_0.1)]";
+	};
 </script>
 
 <div
-    class="{getBlurClass()} {getHoverClasses()}
+	class="{getBlurClass()} {getHoverClasses()}
         {getShineSize()}
         {props.class ?? ''}"
-    style="--shine-color: {shineColor}; {isAndroid() ? '-webkit-transform: translate3d(0, 0, 0);' : ''} {props.style || ''}"
-    bind:this={thisElement}
-    {...props.events}
+	style="--shine-color: {shineColor}; {isAndroid()
+		? '-webkit-transform: translate3d(0, 0, 0);'
+		: ''} {props.style || ''}"
+	bind:this={thisElement}
+	{...props.events}
 >
-    {@render children?.()}
+	{@render children?.()}
 </div>
