e9fdcf03d696b60c156544307772c6a25a50c96f
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-01-16 22:18:15 +0700

    feat: reinstall android init

--- scripts/tauri-android-initialize.ts
+++ scripts/tauri-android-initialize.ts
@@ -1,23 +1,22 @@
 import * as path from "path";
 import fs from "fs/promises";
 import { downloadFile } from "./install-helpers";
-import {exit} from "process";
 const version = "6.0";
 const filename = "ffmpeg-kit-main.aar";
 const destDir = path.resolve(
-	"src-tauri",
-	"tauri-plugin-fluyer",
-	"android",
-	"libs",
+    "src-tauri",
+    "tauri-plugin-fluyer",
+    "android",
+    "libs",
 );
 const destPath = path.resolve(destDir, filename);
 const downloadUrl = `https://github.com/alvindimas05/ffmpeg-kit/releases/download/v${version}/${filename}`;
 
 try {
     await fs.access(destPath);
-    console.log("Ffmpeg for Android is already installed");
-    exit();
-} catch {}
+    console.log("Ffmpeg for Android is already installed. Reinstalling...");
+    await fs.rm(destPath);
+} catch { }
 try {
     console.log("Installing Ffmpeg for Android...");
     await fs.mkdir(destDir, { recursive: true });
