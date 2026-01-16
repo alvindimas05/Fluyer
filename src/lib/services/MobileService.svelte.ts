7a081148cd90592f678d1530e1665d0e51016de4
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-01-16 22:57:08 +0700

    fix: mobile stores

--- src/lib/services/MobileService.svelte.ts
+++ src/lib/services/MobileService.svelte.ts
@@ -2,11 +2,18 @@ import {PageRoutes} from "$lib/constants/PageRoutes";
 import {page} from "$app/state";
 import TauriMobileAPI from "$lib/tauri/TauriMobileAPI";
 import {isAndroid} from "$lib/platform";
+import mobileStore from "$lib/stores/mobile.svelte";
 
 const MobileService = {
     initialize: async () => {
         if(!isAndroid()) return;
         MobileService.listenPageEvents();
+        TauriMobileAPI.getNavigationBarHeight().then((height) => {
+            mobileStore.navigationBarHeight = height;
+        });
+        TauriMobileAPI.getStatusBarHeight().then((height) => {
+            mobileStore.statusBarHeight = height;
+        });
     },
     listenPageEvents: () => {
         $effect(() => {
