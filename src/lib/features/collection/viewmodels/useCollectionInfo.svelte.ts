972fdd59751e0b334fcac45edb7d7f29bbfb893c
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-01-22 13:02:07 +0700

    fix: shuffle collection info

--- src/lib/features/collection/viewmodels/useCollectionInfo.svelte.ts
+++ src/lib/features/collection/viewmodels/useCollectionInfo.svelte.ts
@@ -11,76 +11,81 @@ import musicStore from '$lib/stores/music.svelte';
 import { MusicListType } from '$lib/features/music/types';
 import folderStore from '$lib/stores/folder.svelte';
 
+let album = $derived(filterStore.album);
+let showBackButton = $derived.by(async () => {
+	const isNotFolderView = musicStore.listType !== MusicListType.Folder;
+
+	const folderPath = folderStore.currentFolder?.path;
+	const storedPath = await PersistentStoreService.musicPath.get();
+	const isOutsideStoredPath = folderPath ? !storedPath.includes(folderPath) : false;
+
+	return (
+		isNotFolderView || (storedPath.length === 1 && isOutsideStoredPath) || storedPath.length > 1
+	);
+});
+
+let musicList = $derived.by(() => {
+	if (musicStore.listType === MusicListType.Folder) {
+		return FolderService.getMusicList(folderStore.currentFolder);
+	} else if (album) {
+		return LibraryService.sortMusicList(album.musicList);
+	}
+	return [];
+});
+
+let label = $derived.by(() => {
+	if (musicStore.listType === MusicListType.Folder && folderStore.currentFolder) {
+		const folderMusic = FolderService.getMusicList(folderStore.currentFolder);
+		const totalDuration = folderMusic.reduce((acc, music) => acc + music.duration, 0);
+
+		return `${folderStore.currentFolder.path} ${MusicConfig.separator} ${folderMusic.length} ${MusicConfig.separator} ${ProgressService.formatDuration(totalDuration)}`;
+	} else if (album) {
+		return [album.name, album.artist, album.year, album.duration]
+			.filter(Boolean)
+			.join(` ${MusicConfig.separator} `);
+	}
+	return null;
+});
+
+async function handleBack() {
+	if (musicStore.listType === MusicListType.Folder) {
+		const musicPaths = await PersistentStoreService.musicPath.get();
+		if (musicPaths.includes(folderStore.currentFolder!!.path)) {
+			folderStore.currentFolder = null;
+			return;
+		}
+		await FolderService.navigateToParent(folderStore.currentFolder);
+	} else {
+		filterStore.album = null;
+	}
+}
+
+async function addMusicListAndPlay() {
+	await QueueService.resetAndAddList(musicList);
+	if (!musicStore.isPlaying) MusicPlayerService.play();
+}
+
+async function addMusicList() {
+	await QueueService.addList(musicList);
+	const label =
+		musicStore.listType === MusicListType.Folder && folderStore.currentFolder
+			? folderStore.currentFolder.path
+			: album
+				? `${album.name} ${MusicConfig.separatorAlbum} ${album.artist}`
+				: null;
+	ToastService.info(`Added music list to queue: ${label}`);
+}
+
+async function playShuffle() {
+	await MusicPlayerService.pause();
+
+	await QueueService.resetAndAddList(await LibraryService.shuffleMusicList(musicList));
+
+	await MusicPlayerService.play();
+	ProgressService.start();
+}
+
 export default function useCollectionInfo() {
-	let album = $derived(filterStore.album);
-	let showBackButton = $derived.by(async () => {
-		const isNotFolderView = musicStore.listType !== MusicListType.Folder;
-
-		const folderPath = folderStore.currentFolder?.path;
-		const storedPath = await PersistentStoreService.musicPath.get();
-		const isOutsideStoredPath = folderPath ? !storedPath.includes(folderPath) : false;
-
-		return (
-			isNotFolderView || (storedPath.length === 1 && isOutsideStoredPath) || storedPath.length > 1
-		);
-	});
-
-	let musicList = $derived.by(() => {
-		if (musicStore.listType === MusicListType.Folder) {
-			return FolderService.getMusicList(folderStore.currentFolder);
-		} else if (album) {
-			return LibraryService.sortMusicList(album.musicList);
-		}
-		return [];
-	});
-
-	let label = $derived.by(() => {
-		if (musicStore.listType === MusicListType.Folder && folderStore.currentFolder) {
-			const folderMusic = FolderService.getMusicList(folderStore.currentFolder);
-			const totalDuration = folderMusic.reduce((acc, music) => acc + music.duration, 0);
-
-			return `${folderStore.currentFolder.path} ${MusicConfig.separator} ${folderMusic.length} ${MusicConfig.separator} ${ProgressService.formatDuration(totalDuration)}`;
-		} else if (album) {
-			return [album.name, album.artist, album.year, album.duration]
-				.filter(Boolean)
-				.join(` ${MusicConfig.separator} `);
-		}
-		return null;
-	});
-
-	async function handleBack() {
-		if (musicStore.listType === MusicListType.Folder) {
-			const musicPaths = await PersistentStoreService.musicPath.get();
-			if (musicPaths.includes(folderStore.currentFolder!!.path)) {
-				folderStore.currentFolder = null;
-				return;
-			}
-			await FolderService.navigateToParent(folderStore.currentFolder);
-		} else {
-			filterStore.album = null;
-		}
-	}
-
-	async function addMusicListAndPlay() {
-		await QueueService.resetAndAddList(musicList);
-		if (!musicStore.isPlaying) MusicPlayerService.play();
-	}
-
-	async function addMusicList() {
-		await QueueService.addList(musicList);
-		const label =
-			musicStore.listType === MusicListType.Folder && folderStore.currentFolder
-				? folderStore.currentFolder.path
-				: album
-					? `${album.name} ${MusicConfig.separatorAlbum} ${album.artist}`
-					: null;
-		ToastService.info(`Added music list to queue: ${label}`);
-	}
-
-	function playShuffle() {
-		return MusicPlayerService.playShuffle(musicList);
-	}
-
 	return {
 		get album() {
 			return album;
