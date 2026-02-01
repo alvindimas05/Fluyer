0a48d8ab1c9fb0ed4810f7dd42f4b4e6848729cc
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-02-01 16:14:08 +0700

    fix: unnecessary args on linux

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,5 +5,6 @@ fn main() {
     if !std::env::var("TARGET").unwrap().contains("android") {
         println!("cargo:rustc-link-search=native={}", "libs");
     }
+
     tauri_build::build()
 }
--- src-tauri/src/platform.rs
+++ src-tauri/src/platform.rs
@@ -21,26 +21,4 @@ pub fn is_ios() -> bool {
 pub fn is_mobile() -> bool {
     is_android() || is_ios()
 }
-
-pub fn init() {
-    #[cfg(target_os = "linux")]
-    {
-        // Set chromium as the webview backend
-        std::env::set_var("WRY_WEBVIEW_BACKEND", "chromium");
-        std::env::set_var("TAURI_WEBVIEW_BACKEND", "chromium");
-        std::env::set_var("WEBVIEW_BACKEND", "chromium");
-
-        // Enable hardware acceleration
-        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "0");
-        std::env::set_var("WEBKIT_FORCE_HARDWARE_ACCELERATION", "1");
-
-        // Chromium detection flags
-        std::env::set_var(
-            "CHROME_DEVEL_SANDBOX",
-            "/usr/lib/chromium-browser/chrome-sandbox",
-        );
-        std::env::set_var("CHROME_WRAPPER", "/usr/bin/chromium-browser");
-    }
-}
-
 // Re-export platform-specific constants and functions
