b37a9eea8118d3610c9b820460b76d247a00dab5
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-02-05 01:09:03 +0700

    fix: linux development bass

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,12 @@
 fn main() {
     #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/lib/fluyer");
+    {
+        // Production rpath for installed packages
+        println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/lib/fluyer");
+        // Development rpath - $ORIGIN allows finding libs relative to the executable
+        // Binary is at target/debug/fluyer, libs are at libs/
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../../libs");
+    }
 
     if !std::env::var("TARGET").unwrap().contains("android") {
         println!("cargo:rustc-link-search=native={}", "libs");
