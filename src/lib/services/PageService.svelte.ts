4b3b21764368ff30647cfd7f36d122e3dd9c4767
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-02-19 01:22:56 +0700

    fix: full screen on mobile

--- src/lib/services/PageService.svelte.ts
+++ src/lib/services/PageService.svelte.ts
@@ -1,18 +1,19 @@
 import { goto } from '$app/navigation';
 import { page } from '$app/state';
 import { PageRoutes } from '$lib/constants/PageRoutes';
+import { isMobile } from '$lib/platform';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 
 const PageService = {
 	goTo: async (route: string) => {
-		if (route === PageRoutes.PLAY) {
+		if (route === PageRoutes.PLAY && !isMobile()) {
 			await getCurrentWindow().setFullscreen(true);
 			return goto(route);
 		}
 		return goto(route);
 	},
 	back: async () => {
-		if (page.url.pathname === PageRoutes.PLAY) {
+		if (page.url.pathname === PageRoutes.PLAY && !isMobile()) {
 			await getCurrentWindow().setFullscreen(false);
 		}
 		history.back();
