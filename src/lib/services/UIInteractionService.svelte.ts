ffa525e82cfe7bb240e1c60850d78907d98205a1
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-02-22 04:06:45 +0700

    fix: disable scroll overflow text

--- src/lib/services/UIInteractionService.svelte.ts
+++ src/lib/services/UIInteractionService.svelte.ts
@@ -10,20 +10,21 @@ const UIInteractionService = {
 		UIInteractionService.blockDefaultKeyActions();
 		UIInteractionService.handleSpacePlaybackToggle();
 	},
+	// FIXME: Auto scroll overflow text uses a lot of CPU
 	autoScrollOverflowText: () => {
-		const scrollDuration = 3000;
-		let scrollEnd = true;
-		setInterval(() => {
-			let elements = document.querySelectorAll('.animate-scroll-overflow-text');
-			for (let i = elements.length; i > 0; --i) {
-				fluidScroll({
-					scrollingElement: elements[i],
-					xPos: scrollEnd ? 'end' : 'start',
-					duration: scrollDuration
-				});
-			}
-			scrollEnd = !scrollEnd;
-		}, scrollDuration + 2000);
+		// const scrollDuration = 3000;
+		// let scrollEnd = true;
+		// setInterval(() => {
+		// 	let elements = document.querySelectorAll('.animate-scroll-overflow-text');
+		// 	for (let i = elements.length; i > 0; --i) {
+		// 		fluidScroll({
+		// 			scrollingElement: elements[i],
+		// 			xPos: scrollEnd ? 'end' : 'start',
+		// 			duration: scrollDuration
+		// 		});
+		// 	}
+		// 	scrollEnd = !scrollEnd;
+		// }, scrollDuration + 2000);
 	},
 	blockDefaultKeyActions: () => {
 		window.addEventListener('keydown', (e) => {
