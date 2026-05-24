2c78b86c46083fde744a62ad33046f011a891a1c
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-05-24 11:46:44 +0700

    fix: phosphor icons deprecation

--- src/lib/ui/icon/registry/icon-registry-phospor.ts
+++ src/lib/ui/icon/registry/icon-registry-phospor.ts
@@ -1,86 +1,86 @@
 import { IconType } from '$lib/ui/icon/types';
-import ArrowUUpLeft from 'phosphor-svelte/lib/ArrowUUpLeft';
-import Broom from 'phosphor-svelte/lib/Broom';
-import SortAscending from 'phosphor-svelte/lib/SortAscending';
-import SortDescending from 'phosphor-svelte/lib/SortDescending';
-import ChartBar from 'phosphor-svelte/lib/ChartBar';
-import DotsSixVertical from 'phosphor-svelte/lib/DotsSixVertical';
-import Faders from 'phosphor-svelte/lib/Faders';
-import FileText from 'phosphor-svelte/lib/FileText';
-import Folder from 'phosphor-svelte/lib/Folder';
-import FrameCorners from 'phosphor-svelte/lib/FrameCorners';
-import Gear from 'phosphor-svelte/lib/Gear';
-import GridFour from 'phosphor-svelte/lib/GridFour';
-import LockSimple from 'phosphor-svelte/lib/LockSimple';
-import MagnifyingGlass from 'phosphor-svelte/lib/MagnifyingGlass';
-import MusicNote from 'phosphor-svelte/lib/MusicNote';
-import MusicNotes from 'phosphor-svelte/lib/MusicNotes';
-import PauseCircle from 'phosphor-svelte/lib/PauseCircle';
-import PlayCircle from 'phosphor-svelte/lib/PlayCircle';
-import QuestionMark from 'phosphor-svelte/lib/QuestionMark';
-import Queue from 'phosphor-svelte/lib/Queue';
-import Repeat from 'phosphor-svelte/lib/Repeat';
-import RepeatOnce from 'phosphor-svelte/lib/RepeatOnce';
-import Shuffle from 'phosphor-svelte/lib/Shuffle';
-import SkipBackCircle from 'phosphor-svelte/lib/SkipBackCircle';
-import SkipForwardCircle from 'phosphor-svelte/lib/SkipForwardCircle';
-import SpeakerHigh from 'phosphor-svelte/lib/SpeakerHigh';
-import SpeakerX from 'phosphor-svelte/lib/SpeakerX';
-import Trash from 'phosphor-svelte/lib/Trash';
-import VinylRecord from 'phosphor-svelte/lib/VinylRecord';
-import XCircle from 'phosphor-svelte/lib/XCircle';
-import Playlist from 'phosphor-svelte/lib/Playlist';
-import Plus from 'phosphor-svelte/lib/Plus';
-import Check from 'phosphor-svelte/lib/Check';
-import X from 'phosphor-svelte/lib/X';
-import Image from 'phosphor-svelte/lib/Image';
-import List from 'phosphor-svelte/lib/List';
+import ArrowUUpLeftIcon from 'phosphor-svelte/lib/ArrowUUpLeftIcon';
+import BroomIcon from 'phosphor-svelte/lib/BroomIcon';
+import SortAscendingIcon from 'phosphor-svelte/lib/SortAscendingIcon';
+import SortDescendingIcon from 'phosphor-svelte/lib/SortDescendingIcon';
+import ChartBarIcon from 'phosphor-svelte/lib/ChartBarIcon';
+import DotsSixVerticalIcon from 'phosphor-svelte/lib/DotsSixVerticalIcon';
+import FadersIcon from 'phosphor-svelte/lib/FadersIcon';
+import FileTextIcon from 'phosphor-svelte/lib/FileTextIcon';
+import FolderIcon from 'phosphor-svelte/lib/FolderIcon';
+import FrameCornersIcon from 'phosphor-svelte/lib/FrameCornersIcon';
+import GearIcon from 'phosphor-svelte/lib/GearIcon';
+import GridFourIcon from 'phosphor-svelte/lib/GridFourIcon';
+import LockSimpleIcon from 'phosphor-svelte/lib/LockSimpleIcon';
+import MagnifyingGlassIcon from 'phosphor-svelte/lib/MagnifyingGlassIcon';
+import MusicNoteIcon from 'phosphor-svelte/lib/MusicNoteIcon';
+import MusicNotesIcon from 'phosphor-svelte/lib/MusicNotesIcon';
+import PauseCircleIcon from 'phosphor-svelte/lib/PauseCircleIcon';
+import PlayCircleIcon from 'phosphor-svelte/lib/PlayCircleIcon';
+import QuestionMarkIcon from 'phosphor-svelte/lib/QuestionMarkIcon';
+import QueueIcon from 'phosphor-svelte/lib/QueueIcon';
+import RepeatIcon from 'phosphor-svelte/lib/RepeatIcon';
+import RepeatOnceIcon from 'phosphor-svelte/lib/RepeatOnceIcon';
+import ShuffleIcon from 'phosphor-svelte/lib/ShuffleIcon';
+import SkipBackCircleIcon from 'phosphor-svelte/lib/SkipBackCircleIcon';
+import SkipForwardCircleIcon from 'phosphor-svelte/lib/SkipForwardCircleIcon';
+import SpeakerHighIcon from 'phosphor-svelte/lib/SpeakerHighIcon';
+import SpeakerXIcon from 'phosphor-svelte/lib/SpeakerXIcon';
+import TrashIcon from 'phosphor-svelte/lib/TrashIcon';
+import VinylRecordIcon from 'phosphor-svelte/lib/VinylRecordIcon';
+import XCircleIcon from 'phosphor-svelte/lib/XCircleIcon';
+import PlaylistIcon from 'phosphor-svelte/lib/PlaylistIcon';
+import PlusIcon from 'phosphor-svelte/lib/PlusIcon';
+import CheckIcon from 'phosphor-svelte/lib/CheckIcon';
+import XIcon from 'phosphor-svelte/lib/XIcon';
+import ImageIcon from 'phosphor-svelte/lib/ImageIcon';
+import ListIcon from 'phosphor-svelte/lib/ListIcon';
 
 const iconRegistryPhospor = {
-	[IconType.Unknown]: QuestionMark,
-	[IconType.Play]: PlayCircle,
-	[IconType.Pause]: PauseCircle,
-	[IconType.Previous]: SkipBackCircle,
-	[IconType.Next]: SkipForwardCircle,
-	[IconType.Playing]: MusicNotes,
-	[IconType.Note]: MusicNote,
-	[IconType.PlayBack]: ArrowUUpLeft,
-	[IconType.Back]: ArrowUUpLeft,
-	[IconType.AlbumBack]: ArrowUUpLeft,
-	[IconType.Speaker]: SpeakerHigh,
-	[IconType.Mute]: SpeakerX,
-	[IconType.Remove]: XCircle,
-	[IconType.Trash]: Trash,
-	[IconType.Settings]: Gear,
-	[IconType.Search]: MagnifyingGlass,
-	[IconType.QueueMusic]: Queue,
-	[IconType.CleanQueue]: Broom,
-	[IconType.RepeatNone]: Repeat,
-	[IconType.RepeatPlayNone]: Repeat,
-	[IconType.Repeat]: Repeat,
-	[IconType.RepeatOne]: RepeatOnce,
-	[IconType.Shuffle]: Shuffle,
-	[IconType.Fullscreen]: FrameCorners,
-	[IconType.DragOn]: DotsSixVertical,
-	[IconType.DragOff]: LockSimple,
-	[IconType.SaveLog]: FileText,
-	[IconType.Equalizer]: Faders,
-	[IconType.Close]: XCircle,
-	[IconType.Visualizer]: ChartBar,
-	[IconType.Folder]: Folder,
-	[IconType.SortAsc]: SortAscending,
-	[IconType.SortDesc]: SortDescending,
-	[IconType.MusicListTypeAll]: GridFour,
-	[IconType.MusicListTypeAlbum]: VinylRecord,
-	[IconType.MusicListTypeMusic]: MusicNote,
-	[IconType.MusicListTypeFolder]: Folder,
-	[IconType.MusicListTypePlaylist]: Playlist,
-	[IconType.PlaylistAdd]: Plus,
-	[IconType.Check]: Check,
-	[IconType.Cancel]: X,
-	[IconType.Image]: Image,
-	[IconType.Menu]: List,
-	[IconType.Queue]: Queue
+	[IconType.Unknown]: QuestionMarkIcon,
+	[IconType.Play]: PlayCircleIcon,
+	[IconType.Pause]: PauseCircleIcon,
+	[IconType.Previous]: SkipBackCircleIcon,
+	[IconType.Next]: SkipForwardCircleIcon,
+	[IconType.Playing]: MusicNotesIcon,
+	[IconType.Note]: MusicNoteIcon,
+	[IconType.PlayBack]: ArrowUUpLeftIcon,
+	[IconType.Back]: ArrowUUpLeftIcon,
+	[IconType.AlbumBack]: ArrowUUpLeftIcon,
+	[IconType.Speaker]: SpeakerHighIcon,
+	[IconType.Mute]: SpeakerXIcon,
+	[IconType.Remove]: XCircleIcon,
+	[IconType.Trash]: TrashIcon,
+	[IconType.Settings]: GearIcon,
+	[IconType.Search]: MagnifyingGlassIcon,
+	[IconType.QueueMusic]: QueueIcon,
+	[IconType.CleanQueue]: BroomIcon,
+	[IconType.RepeatNone]: RepeatIcon,
+	[IconType.RepeatPlayNone]: RepeatIcon,
+	[IconType.Repeat]: RepeatIcon,
+	[IconType.RepeatOne]: RepeatOnceIcon,
+	[IconType.Shuffle]: ShuffleIcon,
+	[IconType.Fullscreen]: FrameCornersIcon,
+	[IconType.DragOn]: DotsSixVerticalIcon,
+	[IconType.DragOff]: LockSimpleIcon,
+	[IconType.SaveLog]: FileTextIcon,
+	[IconType.Equalizer]: FadersIcon,
+	[IconType.Close]: XCircleIcon,
+	[IconType.Visualizer]: ChartBarIcon,
+	[IconType.Folder]: FolderIcon,
+	[IconType.SortAsc]: SortAscendingIcon,
+	[IconType.SortDesc]: SortDescendingIcon,
+	[IconType.MusicListTypeAll]: GridFourIcon,
+	[IconType.MusicListTypeAlbum]: VinylRecordIcon,
+	[IconType.MusicListTypeMusic]: MusicNoteIcon,
+	[IconType.MusicListTypeFolder]: FolderIcon,
+	[IconType.MusicListTypePlaylist]: PlaylistIcon,
+	[IconType.PlaylistAdd]: PlusIcon,
+	[IconType.Check]: CheckIcon,
+	[IconType.Cancel]: XIcon,
+	[IconType.Image]: ImageIcon,
+	[IconType.Menu]: ListIcon,
+	[IconType.Queue]: QueueIcon,
 };
 
 export default iconRegistryPhospor;
